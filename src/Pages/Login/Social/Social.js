48c296689a1bb7b426a28f292b666963b0a2256e
partho2589 <parthomalo615@.com>
2022-05-06 17:54:38 +0600

    added fack data

--- src/Pages/Login/Social/Social.js
+++ src/Pages/Login/Social/Social.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './Social.css'
 import googleIcon from '../../../image/icon/google-icon.png'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 const Social = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     
+    const navigate = useNavigate();
+    if (user){
+        navigate('/home')
+    }
+    let errorElement;
+    if (error) {
+        
+           errorElement =  <div>
+                <p className='text-danger'>Error: {error?.message}</p>
+            </div>
+       
+    }
     return (
 
         <div>
@@ -11,9 +28,12 @@ const Social = () => {
                 <p className='mt-3 px-2'>Or</p>
                 <div style={{ height: "1px" }} className='bg-info w-50'></div>
             </div>
-
+            <div>
+                {errorElement}
+            </div>
             <div>
                 <button
+                onClick={() => signInWithGoogle}
                    id='google-btn'
                     className=' w-50 d-block mx-auto'>
                     <img src={googleIcon} alt="" />
