9195a02baedc89926c83b3ebe7cd865d5d92fde3
partho2589 <parthomalo615@.com>
2022-05-08 05:06:26 +0600

    added link with manage item route

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Banner from '../Banner/Banner';
 import Products from '../Products/Products';
-
+import './Home.css'
 
 const Home = () => {
+
     return (
         <div>
             <Banner></Banner>
             <Products></Products>
-           
+           <div className=' mt-5 d-flex align-items-center justify-content-center'>
+           <Link className='manage-link ' to='/manage'> Manage Item</Link> 
+           </div>
         </div>
     );
 };
